ae74c0a2435b192349d67dd7f80fe82a12b2be4f
Lamia Fatiha <f6lamia@gmail.com>
2024-07-14 22:50:09 +0600

    post list part done

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
+import { getPosts } from "../services/api";
 
 const PostList = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=> {
+        const fetchPosts = async () => {
+            const posts = await getPosts();
+            setPosts(posts);
+        };
+        fetchPosts();
+    }, []);
+    
   return (
     <div>
-      
+     <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+            <li key={post.id}>
+              <Link to={`/post/${post.id}`}>
+                 {post.title}
+              </Link>
+            </li>
+        ))}
+      </ul>    
     </div>
-  )
-}
+  );
+};
 
-export default PostList
+export default PostList;
