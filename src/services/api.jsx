8a4c555e2851d875fbf596dbbe9667a0dcc58642
Lamia Fatiha <f6lamia@gmail.com>
2024-07-14 22:38:40 +0600

    functions to interact with the JSONPlaceholder API using Axios

---
+++ src/services/api.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
+export const getPosts = async () => {
+    const response = await api.get('/posts');
+    return response.data;
+};
+
+export const getPostsById = async (id) => {
+    const response = await api.get(`/posts/${id}`);
+    return response.data;
+};
